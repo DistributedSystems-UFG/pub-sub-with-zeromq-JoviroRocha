4df62099f1d74f949692bf362d7e53153a357007
Joviro Rocha <joviro_neto@hotmail.com>
2023-01-08 12:04:24 -0800

    A

--- Client.py
+++ Client.py
@@ -5,24 +5,27 @@ import rpyc
 
 class SubscriberHandler(threading.Thread):
   def __init__(self):
-        threading.Thread.__init__(self)
-        context = zmq.Context()
-        s = context.socket(zmq.SUB)          # create a subscriber socket
-        p = "tcp://"+ constPS.HOST +":"+ constPS.PORT        # how and where to communicate
-        s.connect(p)                         # connect to the server
-        print("Choose the topics that you want to subscribe: \nThe following topics are available: ")
-        for item in constPS.registry_topics:
-	        print(item)
-        topics = input("Enter all topics you want to subscribe to on the same line and separated by a space: ").split()
-        for item in topics:
-	        s.setsockopt_string(zmq.SUBSCRIBE, item)  # subscribe to TIME messages
-        while True:
-	        message = s.recv()   # receive a message
-	        print (bytes.decode(message))
+    threading.Thread.__init__(self)
 
+  def run(self):  
+      message = s.recv()   # receive a message
+      print (bytes.decode(message))
+
+context = zmq.Context()
+s = context.socket(zmq.SUB)          # create a subscriber socket
+p = "tcp://"+ constPS.HOST +":"+ constPS.PORT_ZEROMQ        # how and where to communicate
+s.connect(p)                         # connect to the server
+print("Choose the topics that you want to subscribe: \nThe following topics are available: ")
+for item in constPS.registry_topics:
+  print(item)
+topics = input("Enter all topics you want to subscribe to on the same line and separated by a space: ").split()
+for item in topics:
+  print(item)
+  s.setsockopt_string(zmq.SUBSCRIBE, item)  # subscribe to TIME messages
+    
 subscriber_thread = SubscriberHandler()
 subscriber_thread.start()
-conn = rpyc.connect(constPS.HOST, constPS.PORT)
+conn = rpyc.connect(constPS.HOST, constPS.PORT_RPYC)
 while True:
     topic = input("Enter the topic: ")
     message = input("Enter the message: ")
--- Server.py
+++ Server.py
@@ -4,10 +4,15 @@ import threading
 import rpyc
 from rpyc.utils.server import ThreadedServer
 
-def exposed_publish(self, topic, message):
-	context = zmq.Context()
+class Chating(rpyc.Service):
+    def exposed_publish(self, topic, message):      
+        msg = str.encode(topic + " " + message)
+        s.send(msg) # publish the current time
+   
+if __name__ == "__main__":
+    context = zmq.Context()
     s = context.socket(zmq.PUB)        # create a publisher socket
-    p = "tcp://"+ constPS.HOST +":"+ constPS.PORT      # how and where to communicate
-    s.bind(p)        
-    msg = str.encode(topic + " " + message)
-	s.send(msg) # publish the current time 
+    p = "tcp://"+ constPS.HOST +":"+ constPS.PORT_ZEROMQ      # how and where to communicate
+    s.bind(p)
+    server = ThreadedServer(Chating(), port = constPS.PORT_RPYC)
+    server.start()
